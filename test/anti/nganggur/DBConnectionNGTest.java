d9c173b869e57e3102ebc954af86e376ce066311
akhmadwhd <32125215+akhmadwhd@users.noreply.github.com>
2021-07-04 14:32:02 +0700

    Coding Standard
    Menghapus Comment Yang Tidak Penting Dari Codingan

--- test/anti/nganggur/DBConnectionNGTest.java
+++ test/anti/nganggur/DBConnectionNGTest.java
@@ -1,51 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package anti.nganggur;
 
 import java.sql.Connection;
 import static org.testng.Assert.*;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-/**
- *
- * @author akhma
- */
 public class DBConnectionNGTest {
     
     public DBConnectionNGTest() {
     }
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
-    @BeforeMethod
-    public void setUpMethod() throws Exception {
-    }
-
-    @AfterMethod
-    public void tearDownMethod() throws Exception {
-    }
-
-    /**
-     * Test of getConnection method, of class DBConnection.
-     */
     @Test
     public void testGetConnection() {
         System.out.println("getConnection");
         Connection result = DBConnection.getConnection();
         assertEquals(result!= null,true);
+        
+    }
+    
+    @Test
+    public void test() {
         
     }
     
