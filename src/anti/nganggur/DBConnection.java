dd7d6081bc9689e69c39bfe7b741cb042cd6c49b
akhmadwhd <32125215+akhmadwhd@users.noreply.github.com>
2021-07-04 14:51:31 +0700

    Update Error Pada MyJob Freelance
    Sebelumnya data yang diambil oleh freelancer tidak dapat ditampilkan

--- src/anti/nganggur/DBConnection.java
+++ src/anti/nganggur/DBConnection.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package anti.nganggur;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
@@ -11,10 +6,6 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author akhma
- */
 public class DBConnection {
     private static final String servername = "localhost";
     private static final String username = "root";
